4b1374b5c5ebb5aa3be8b55596f1b0b2a895756e
Viilhov <133661770+Viilhov@users.noreply.github.com>
2023-10-07 12:50:00 +0200

    Created a form which accept first and last name

--- myreact/src/App.jsx
+++ myreact/src/App.jsx
@@ -1,12 +1,13 @@
 import ConditionalComponent from "./ConditionalComponent";
 import Counter from "./Counter";
+import Form from "./Form";
 import Fruits from "./Fruits";
 import Message from "./Message";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Form />
     </div>
   );
 }
